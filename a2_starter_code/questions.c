0771c53062485e8210ed25ad237c0dbd564a8c40
Maxwell Fang <maxwellfang@Maxwells-MacBook-Pro.local>
2016-11-01 16:20:04 -0400

    Task 1 completed

--- a2_starter_code/questions.c
+++ a2_starter_code/questions.c
@@ -3,24 +3,29 @@
 #include <stdlib.h>
 #include "questions.h"
 
-#define N 129
 Node * get_list_from_file (char *input_file_name) {
 	Node * head = NULL;
-	
+	Node * tail = NULL;
     //read lines and add to the list
-    File *input_file=fopen(input_file_name, "r");
+    FILE *input_file=fopen(input_file_name, "r");
     if(input_file==NULL) {
     	fprintf(stderr, "Error opening file.\n");
     }
-    head = malloc(sizeof(Node));
-    Node * tail = head;
-    char line[N];
-    while(fgets(line, N, input_file)!=NULL) {
+    char line[MAX_LINE];
+    while(fgets(line, MAX_LINE, input_file)!=NULL) {
     	line[strcspn(line, "\r\n")] = '\0';
-    	tail->str = malloc(sizeof(char)*(strlen(line) + 1));
-    	tail->str = line;
-    	tail->next = malloc(sizeof(Node));
-    	tail = tail->next;
+        Node * temp = malloc(sizeof(Node));
+    	temp->str = malloc(sizeof(char)*(strlen(line) + 1));
+    	strcpy(temp->str, line);
+        temp->next = NULL;
+        if(head == NULL) {
+            head = temp;
+            tail = temp;
+        }
+        else {
+            tail->next = temp;
+            tail = tail->next;
+        }
     }
     int error = fclose(input_file);
     if(error == 1) {
@@ -33,11 +38,14 @@ void print_list (Node *head) {
 	Node *temp = head;
 	while(temp != NULL) {
 		printf("%s\n", temp->str);
+        temp = temp->next;
 	}
 }
 
 void free_list (Node *head) {
-	free_list(head->next);
-	free(head->str);
-	free(head);
+    if(head!=NULL) {
+	   free_list(head->next);
+	   free(head->str);
+	   free(head);
+    }
 }
